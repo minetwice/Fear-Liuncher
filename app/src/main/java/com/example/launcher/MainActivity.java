5bafb7118f90660de7f57f54af3fdee245e4dee4
Twicefear <ytd82774@gmail.com>
2026-05-14 23:41:56 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/launcher/MainActivity.java
+++ app/src/main/java/com/example/launcher/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.launcher;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, SetupActivity.class));
+        finish();
     }
 }
