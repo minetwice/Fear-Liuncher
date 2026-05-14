173d538a5a99ab949e74965df706213bfec50d41
Twicefear <ytd82774@gmail.com>
2026-05-14 23:34:03 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,10 +1,8 @@
 @ECHO OFF
 
-SET DIRNAME=%~dp0
-SET APP_HOME=%DIRNAME%
+set APP_HOME=%~dp0
+set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
 
-SET DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
-
-SET CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 java %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
