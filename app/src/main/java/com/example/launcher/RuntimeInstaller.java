e72cb87d3c9d743316567711b484355f89536105
Twicefear <ytd82774@gmail.com>
2026-05-15 05:49:01 +0530

    Create RuntimeInstaller.java

---
+++ app/src/main/java/com/example/launcher/RuntimeInstaller.java
@@ -0,0 +1,17 @@
+package com.example.launcher;
+
+import android.util.Log;
+
+public class RuntimeInstaller {
+
+    public static void installAll() {
+
+        Log.d("FearLauncher", "Installing Java 17...");
+        Log.d("FearLauncher", "Installing Java 21...");
+        Log.d("FearLauncher", "Installing Java 25...");
+
+        Log.d("FearLauncher", "Installing LWJGL...");
+        Log.d("FearLauncher", "Installing JNI...");
+        Log.d("FearLauncher", "Installing Runtime...");
+    }
+}
